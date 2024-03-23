c35eeb3fee8bd506bca73505daa9c00419c379fc
JamahaW <pret2titio1n@gmail.com>
2024-03-23 17:09:33 +0300

    line sensor change

--- linesensor.hpp
+++ linesensor.hpp
@@ -7,7 +7,20 @@
 
 /// @brief Аналоговый Датчик линии
 class LineSensor {
+
+private:
+
     const uint8_t PIN;
-    const uint16_t on_line;
-    const uint16_t on_field;
+    const uint16_t ON_LINE;
+    const uint16_t ON_FIELD;
+
+public:
+
+    LineSensor(
+        const uint8_t pin,
+        const uint16_t on_line,
+        const uint16_t on_field
+    ) : PIN(pin), ON_LINE(on_line), ON_FIELD(on_field) {
+        pinMode(PIN, INPUT);
+    }
 };
