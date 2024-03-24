bb153a55c662188de872c8dd5daacb73362bf041
JamahaW <pret2titio1n@gmail.com>
2024-03-24 04:13:00 +0300

    Добавлены тесты моторов: по ШИМ или Контролю скорости

--- src/MoshLib.hpp
+++ src/MoshLib.hpp
@@ -1,6 +1,5 @@
 // Библиотека для робота МОШ (Jamaha)
 
-
 #pragma once
 
 #include "config.hpp"
@@ -10,8 +9,7 @@
 
 
 // ПИНЫ
-namespace pin
-{
+namespace pin {
     // MOTOРЫ
     enum MOTOR {
         // ЛЕВЫЙ
@@ -29,49 +27,43 @@ namespace pin
         MR_ENC_B = 9, // ЗЕЛЁНЫЙ
     };
 
-    // ДАТЧИКИ ЛИНИИ
-    enum LINES {
-        L_SENSOR = A0,
-        R_SENSOR = A1,
-    };
-
-    // ИК-ДАТЧИК РАССТОЯНИЯ
-    enum IR {
-        L_PIN = A2,
-        R_PIN = A3,
-    };
-
-    // УЗ-ДАТЧИК РАССТОЯНИЯ
-    enum US {
-        PIN_ECHO = 12,
-        PIN_TRIG = 13,
-    };
+    enum LINES { L_SENSOR = A0, R_SENSOR = A1, }; // ДАТЧИКИ ЛИНИИ
+    enum IR { L_PIN = A2, R_PIN = A3, }; // ИК-ДАТЧИК РАССТОЯНИЯ
+    enum US { PIN_ECHO = 12, PIN_TRIG = 13, }; // УЗ-ДАТЧИК РАССТОЯНИЯ
 }
 
 /// @brief Левый датчик линии
-hardware::LineSensor lineL(pin::L_SENSOR, PARAMS::L_LINE, PARAMS::L_FIELD);
+extern hardware::LineSensor lineL;
 
 /// @brief Правый датчик линии
-hardware::LineSensor lineR(pin::R_SENSOR, PARAMS::R_LINE, PARAMS::R_FIELD);
+extern hardware::LineSensor lineR;
 
 /// @brief Левый ИК-датчик расстояния
-hardware::IrSensorSharp irL(pin::L_PIN);
+extern hardware::IrSensorSharp irL;
 
 /// @brief Правый ИК-датчик расстояния
-hardware::IrSensorSharp irR(pin::R_PIN);
+extern hardware::IrSensorSharp irR;
 
 /// @brief Ультразвуковой датчик расстояния
-hardware::UltraSonic us(pin::PIN_ECHO, pin::PIN_TRIG);
+extern hardware::UltraSonic us;
 
 /// @brief Левый мотор
-hardware::MotorEncoder motorL(hardware::__l_int, pin::ML_INVERT, pin::ML_SPEED, pin::ML_DIR, pin::ML_ENC_A, pin::ML_ENC_B);
+extern hardware::MotorEncoder motorL;
 
-/// @brief Праавый мотор
-hardware::MotorEncoder motorR(hardware::__r_int, pin::MR_INVERT, pin::MR_SPEED, pin::MR_DIR, pin::MR_ENC_A, pin::MR_ENC_B);
+/// @brief Правый мотор
+extern hardware::MotorEncoder motorR;
 
-void hardware::__l_int() { motorL.enc(); }
-void hardware::__r_int() { motorR.enc(); }
 
+// ФУНКЦИИ ДВИЖЕНИЯ
+
+/**
+ * @brief Включить моторы с заданой скоростью на время
+ * @param speed_L скорость левого мотора
+ * @param speed_R скорость правого мотора
+ * @param runtime время работы в мс
+ * @param stop_at_exit Выключение ШИМ после завершения движения (= true)
+ */
+void setMotorsTime(int8_t speed_L, int8_t speed_R, uint32_t runtime, bool stop_at_exit = true);
 
 /**
  * @brief Удерживать моторы в текущем положении на время
@@ -79,85 +71,28 @@ void hardware::__r_int() { motorR.enc(); }
  */
 void goHold(uint32_t timeout = PARAMS::HOLD_STOP_TIMEOUT);
 
-// управление моторами
-namespace motors
-{
-    void reset() {
-        motorL.reset();
-        motorR.reset();
-    }
-
-    void spin() {
-        motorL.spin();
-        motorR.spin();
-    }
-
-    void setSpeeds(int8_t left, int8_t right) {
-        motorL.setSpeed(left);
-        motorR.setSpeed(right);
-    }
-
-    void setForTime(int8_t speed, uint32_t runtime_ms) {
-        uint32_t end_time = millis() + runtime_ms;
-        setSpeeds(speed, speed);
-        while (millis() < end_time) spin();
-        motorL.setPWM(0);
-        motorR.setPWM(0);
-    }
-
-    void setForTicks(int8_t speed_L, int32_t ticks_L, int8_t speed_R, int32_t ticks_R) {
-        reset();
-        setSpeeds(speed_L, speed_R);
-        motorL.target = ticks_L;
-        motorR.target = ticks_R;
-
-        bool runL = true;
-        bool runR = true;
-
-        while (runL || runR) {
-            runL = motorL.follow();
-            runR = motorR.follow();
-        }
-
-        goHold();
-    }
-} // namespace motors
-
-void goHold(uint32_t timeout) { motors::setForTime(0, timeout); }
-
-
 /**
  * @brief Проехать 1000 тиков вперёд прямо
  * @param speed скорость движения
  */
-void go_1000_ticks(uint8_t speed = PARAMS::DEFAULT_SPEED) { motors::setForTicks(speed, 1000, speed, 1000); }
-
-
-// Переводной макрос из ММ пути в тики энкодера
-#define MM2TICKS(mm) ( (int32_t)(mm) * 1000L / (int32_t)(PARAMS::MM_IN_1000_TICKS) ) // .. где мой constexpr, Ардуино!!? где С++11???
-
+void go_1000_ticks(uint8_t speed = PARAMS::DEFAULT_SPEED);
 
 /**
  * @brief Прямолинейное движение
  * @param distance_mm расстояние (отрицательное - едет назад)
  * @param speed скорость движения
  */
-void goDirect(int32_t distance_mm, uint8_t speed = PARAMS::DEFAULT_SPEED) {
-    int32_t ticks = MM2TICKS(distance_mm);
-    motors::setForTicks(speed, ticks, speed, ticks);
-}
+void goDirect(int32_t distance_mm, uint8_t speed = PARAMS::DEFAULT_SPEED);
 
 /**
  * @brief Поворот на заданый угол вокруг центра
  * @param a угол поворота ( <0 - поворот против часовой)
  * @param speed скорось поворота
  */
-void turnAngle(int16_t a, uint8_t speed = PARAMS::DEFAULT_SPEED) {
-    int32_t ticks = MM2TICKS((int32_t)a * PARAMS::TRACK * M_PI / 360.0);
-    motors::setForTicks(speed, ticks, speed, -ticks);
-}
+void turnAngle(int16_t a, uint8_t speed = PARAMS::DEFAULT_SPEED);
 
-// ТЕСТ КОМПОНЕНТОВ
+
+// ТЕСТЫ
 namespace test {
 
     /**
@@ -166,18 +101,13 @@ namespace test {
      * @param ret_R возврат для правого
      * @param timeout время измерения
      */
-    void lines(uint16_t& ret_L, uint16_t& ret_R, uint32_t timeout = 1000) {
-        uint16_t i;
-        uint32_t sum_L = 0, sum_R = 0;
-        timeout += millis();
+    void lines(uint16_t& ret_L, uint16_t& ret_R, uint32_t timeout = 1000);
 
-        for (i = 0; millis() < timeout; i++) {
-            sum_L += lineL.readRaw();
-            sum_R += lineR.readRaw();
-        }
+    /// @brief функциональности моторов
+    void motors_accel();
 
-        ret_L = sum_L / i;
-        ret_R = sum_R / i;
-    }
+    /// @brief функциональности моторов (ШИМ ВЕРСИЯ)
+    void motors_pwm();
 
-} // namespace test
+
+}
--- src/moshlib.cpp
+++ src/moshlib.cpp
@@ -1,2 +1,140 @@
 #include "MoshLib.hpp"
 
+
+// КОМПОНЕНТЫ
+
+hardware::LineSensor lineL(pin::L_SENSOR, PARAMS::L_LINE, PARAMS::L_FIELD);
+hardware::LineSensor lineR(pin::R_SENSOR, PARAMS::R_LINE, PARAMS::R_FIELD);
+
+hardware::IrSensorSharp irL(pin::L_PIN);
+hardware::IrSensorSharp irR(pin::R_PIN);
+
+hardware::UltraSonic us(pin::PIN_ECHO, pin::PIN_TRIG);
+
+hardware::MotorEncoder motorL(hardware::__l_int, pin::ML_INVERT, pin::ML_SPEED, pin::ML_DIR, pin::ML_ENC_A, pin::ML_ENC_B);
+hardware::MotorEncoder motorR(hardware::__r_int, pin::MR_INVERT, pin::MR_SPEED, pin::MR_DIR, pin::MR_ENC_A, pin::MR_ENC_B);
+
+
+void hardware::__l_int() { motorL.enc(); }
+void hardware::__r_int() { motorR.enc(); }
+
+// управление моторами
+namespace motors
+{
+    void reset() {
+        motorL.reset();
+        motorR.reset();
+    }
+
+    void spin() {
+        motorL.spin();
+        motorR.spin();
+    }
+
+    void setSpeeds(int8_t left, int8_t right) {
+        motorL.setSpeed(left);
+        motorR.setSpeed(right);
+    }
+
+    void setForTicks(int8_t speed_L, int32_t ticks_L, int8_t speed_R, int32_t ticks_R) {
+        reset();
+        setSpeeds(speed_L, speed_R);
+        motorL.target = ticks_L;
+        motorR.target = ticks_R;
+
+        bool runL = true;
+        bool runR = true;
+
+        while (runL || runR) {
+            runL = motorL.follow();
+            runR = motorR.follow();
+        }
+
+        goHold();
+    }
+} // namespace motors
+
+void setMotorsTime(int8_t speed_L, int8_t speed_R, uint32_t runtime, bool stop_at_exit) {
+    runtime += millis();
+    motors::setSpeeds(speed_L, speed_R);
+    while (millis() < runtime) motors::spin();
+    if (!stop_at_exit) return;
+    motorL.setPWM(0);
+    motorR.setPWM(0);
+}
+
+void goHold(uint32_t timeout) { setMotorsTime(0, 0, timeout); }
+
+void go_1000_ticks(uint8_t speed) { motors::setForTicks(speed, 1000, speed, 1000); }
+
+// Переводной макрос из ММ пути в тики энкодера
+#define MM2TICKS(mm) ( (int32_t)(mm) * 1000L / (int32_t)(PARAMS::MM_IN_1000_TICKS) ) // .. где мой constexpr, Ардуино!!? где С++11???
+
+void goDirect(int32_t distance_mm, uint8_t speed) {
+    int32_t ticks = MM2TICKS(distance_mm);
+    motors::setForTicks(speed, ticks, speed, ticks);
+}
+
+void turnAngle(int16_t a, uint8_t speed) {
+    int32_t ticks = MM2TICKS((int32_t)a * PARAMS::TRACK * M_PI / 360.0);
+    motors::setForTicks(speed, ticks, speed, -ticks);
+}
+
+// ТЕСТИРОВАНИЕ
+
+void test::lines(uint16_t& ret_L, uint16_t& ret_R, uint32_t timeout) {
+    uint16_t i;
+    uint32_t sum_L = 0, sum_R = 0;
+    timeout += millis();
+
+    for (i = 0; millis() < timeout; i++) {
+        sum_L += lineL.readRaw();
+        sum_R += lineR.readRaw();
+    }
+
+    ret_L = sum_L / i;
+    ret_R = sum_R / i;
+}
+
+
+/**
+ * @brief Изменение скорости регулятором
+ * @param motor
+ * @param dir
+ */
+static void test_motors_speed(hardware::MotorEncoder* motor, int8_t dir) {
+    uint32_t timer = millis() + 120;
+
+    motor->reset();
+
+    for (int16_t speed = -PARAMS::MAX_DELTA_TICKS; speed <= PARAMS::MAX_DELTA_TICKS; speed++) {
+        motor->setSpeed((PARAMS::MAX_DELTA_TICKS - abs(speed)) * dir);
+        while (millis() < timer) motor->spin();
+        timer += 120;
+    }
+    motor->setPWM(0);
+    delay(500);
+}
+
+void test::motors_accel() {
+    hardware::MotorEncoder* _motors[]{ &motorL, &motorR };
+
+    for (hardware::MotorEncoder* m : _motors) {
+        test_motors_speed(m, 1);
+        test_motors_speed(m, -1);
+    }
+}
+
+void test::motors_pwm() {
+    hardware::MotorEncoder* _motors[]{ &motorL, &motorR };
+
+    for (hardware::MotorEncoder* motor : _motors) {
+        for (int16_t i = -255; i < 256; i++) {
+            motor->setPWM(255 - abs(i));
+            motor->setDir(i > 0);
+            delay(2 * 2000 / 256);
+        }
+        motor->setPWM(0);
+        delay(500);
+    }
+}
